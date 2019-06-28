faffa939101b41584103a8a4c0e4e808197bc89d
Djaro Donk <41857903+DjaroDonk@users.noreply.github.com>
2019-06-28 18:37:11 +0200

    now added the blacklist function
    it is almost reaching a usable state, I only have to add the scanning comments part

--- bot.py
+++ bot.py
@@ -20,3 +20,27 @@ else:
        comments_replied_to = f.read()
        comments_replied_to = comments_replied_to.split("\n")
        comments_replied_to = list(filter(None, comments_replied_to))
+
+if not os.path.isfile("blacklist.txt"):
+    blacklist = []
+else:
+    with open("blacklist.txt", "r") as f:
+       blacklist = f.read()
+       blacklist = blacklist.split("\n")
+       blacklist = list(filter(None, blacklist))
+
+def blacklist_person(name):
+    global blacklist
+    blacklist.append(name)
+    with open("blacklist.txt","w") as f:
+        f.write(name)
+        f.close()
+
+def scancomment(c):
+    for instruction in instructions:
+        if instruction[0] == 0:
+            if c.body == instruction[1]:
+                if instruction[2] == 0:
+                    c.reply(instructions[3])
+                elif instruction[2] == 2:
+                    c.reply(instructions[3])
