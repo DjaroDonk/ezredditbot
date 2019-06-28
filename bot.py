b5a4c9a913a05b43ac446b9f8fb3383b9d2011a8
Djaro Donk <41857903+DjaroDonk@users.noreply.github.com>
2019-06-28 18:20:38 +0200

    can now log in
    you can now log in

--- bot.py
+++ bot.py
@@ -1,7 +1,15 @@
 import praw
 import parserforbot
+import os
 instructions=parserforbot.instr_list
 config=parserforbot.config
+privateinfo=parserforbot.privateinfo
+theBot = praw.Reddit(client_id=privateinfo["client_id"],
+                     client_secret=privateinfo["client_secret"],
+                     password=privateinfo["password"],
+                     user_agent=privateinfo["user_agent"],
+                     username=privateinfo["username"])
+
 import sys
 non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
 
